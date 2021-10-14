d0644090b21223c12da6e6f3f75bca3ec0d34528
unknown <you@example.com>
2021-10-14 15:12:41 +0800

    V0.9

--- whale.m
+++ whale.m
@@ -1,14 +1,14 @@
 %fig = uifigure;
 %s = uislider(fig);
-fig = uifigure;
-sld = uislider(fig);
-sld.Limits = [0 0.00001];
-sld.Value = 0.000005;
+%fig = uifigure;
+%sld = uislider(fig);
+%sld.Limits = [0 0.00001];
+%sld.Value = 0.000005;
 
-a = sld.Value;
+a = 0.0000001;
 x1 = 5000;
 x2 = 7000;
-k = 1000;
+k =4000;
 t = 0:1:k;
 d = 0.005;%精度参数
 
@@ -20,7 +20,7 @@ k1 = 2;
 x3(1) = x1;
 x4(1) = x2; 
 
-while(k1<k)
+while(k1<k+2)
 
 x3(k1) = x3(k1-1) + (x3(k1-1)*(0.05*x3(k1-1)*(1 - x3(k1-1)/150000) - a*x3(k1-1)*x4(k1-1))/100)*d;
 
@@ -30,12 +30,8 @@ k1 = k1+1;
 
 end
 
-fig = uifigure;
-sld = uislider(fig);
-sld.Limits = [0 0.00001];
-sld.Value = 0.000005;
 
 title "x3(t)"
-plot(t,x4);   
+plot(x3,x4);   
     %%图片不能传上来，效果成功，调整精度值可以获得更严格的结果
 
